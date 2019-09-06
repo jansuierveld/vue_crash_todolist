f0103725906af50ad1b4ef123a7029b0ab13f395
jansuierveld <suierveld.j@tkppensioen.nl>
2019-09-06 14:47:24 +0200

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p> This is the todoList app v1.0.0. Part of 
+      <a href=" https://www.youtube.com/watch?v=Wy9q22isx3U" target="_blank">youtube course</a>
+    </p>  
+  </div>
+</template>
